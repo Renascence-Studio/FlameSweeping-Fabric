fd4735343a53a274307bd0a2f4e5229824ac9bde
chemiofitor <783626185@qq.com>
2023-01-13 00:23:22 +0800

    Little Fix

--- src/main/java/hlft/fabric/flamesweeping/mixin/FireAspectEnchantmentMixin.java
+++ src/main/java/hlft/fabric/flamesweeping/mixin/FireAspectEnchantmentMixin.java
@@ -1,14 +1,15 @@
 package hlft.fabric.flamesweeping.mixin;
 
 import net.minecraft.enchantment.Enchantment;
-import net.minecraft.item.ItemStack;
+import net.minecraft.item.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Enchantment.class)
-public class FireAspectEnchantmentMixin {
+public abstract class FireAspectEnchantmentMixin {
+
     @Inject(at = @At("HEAD"), method = "isAcceptableItem", cancellable = true)
     public void isAcceptableItemMixin(ItemStack stack, CallbackInfoReturnable<Boolean> cir) {
         if (stack.getItem().isDamageable()) {
