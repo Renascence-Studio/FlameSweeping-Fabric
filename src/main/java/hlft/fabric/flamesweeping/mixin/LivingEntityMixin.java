5cfb42e07daf3e36d885c7e096a115652a3c5052
chemiofitor <783626185@qq.com>
2023-07-08 16:06:28 +0800

    1.1 update

--- src/main/java/hlft/fabric/flamesweeping/mixin/LivingEntityMixin.java
+++ src/main/java/hlft/fabric/flamesweeping/mixin/LivingEntityMixin.java
@@ -1,9 +1,9 @@
 package hlft.fabric.flamesweeping.mixin;
 
 import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.damage.DamageSource;
-import net.minecraft.entity.damage.DamageTracker;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,16 +12,16 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Shadow public abstract DamageTracker getDamageTracker();
+
+    @Shadow public abstract boolean isAlive();
 
     @Inject(at = @At("HEAD"), method = "applyDamage")
-    protected void applyDamage(DamageSource source, float amount, CallbackInfo ci) {
-        if(source.getAttacker() instanceof LivingEntity && source.getSource() != null) {
+    protected void damage(DamageSource source, float amount, CallbackInfo ci) {
+        if(source.getAttacker() instanceof LivingEntity) {
             int k = EnchantmentHelper.getFireAspect((LivingEntity) source.getAttacker());
-            DamageTracker tracker = getDamageTracker();
-            if (tracker.getEntity().isAlive()) {
-                if (k > 0 && !tracker.getEntity().isOnFire()) {
-                    tracker.getEntity().setOnFireFor(4 * k);
+            if(this.isAlive()) {
+                if (k > 0 && !((Entity)(Object)this).isOnFire()) {
+                    ((Entity)(Object)this).setOnFireFor(k * 4);
                 }
             }
         }
